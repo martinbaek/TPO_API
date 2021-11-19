e51750c7e970ddd37dc62658c7e691d7aacd3c09
Martin Baek <tinchobaek@gmail.com>
2021-11-19 14:26:36 -0300

    copia sign up

--- src/SignInPageVieja.js
+++ src/SignInPageVieja.js
@@ -5,9 +5,6 @@ import Button from '@material-ui/core/Button';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import {Link} from "react-router-dom";
 
-import React, { useEffect } from "react";
-import {Redirect} from "react-router-dom";
-
 const useStyles = makeStyles((theme) => ({
     root: {
       display: 'flex',
